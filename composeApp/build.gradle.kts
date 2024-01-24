090e2d42bf732a86bc443e5e3339d16b73b9d1a2
Lek <lek@Leks-MBP.rstechnicalconsulting.com>
2024-01-23 20:36:46 -0500

    upped version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -89,8 +89,8 @@ android {
         applicationId = "com.lekalina.android.memory"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
     packaging {
         resources {
